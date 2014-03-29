64cf86f6b6c197f02f9e62a16598e51d39a66892
Michael Sheinberg <m.sheiny@gmail.com>
2014-03-29 15:01:24 -0400

    Added classifiers and URL metadata

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(name='updog',
         version='1.0',
         description='Network device configuration manager',
         author='Michael Sheinberg',
+        url='https://github.com/msheiny/updog',
         author_email='m.sheiny@gmail.com',
         scripts=['bin/woof','bin/bark'],
         packages=['updog'],
@@ -17,4 +18,12 @@ setup(name='updog',
             'shell>=1.0.1',
             'nose'
             ],
+        classifiers=[
+            'Topic :: System :: Networking :: Monitoring',
+            'Topic :: System :: Systems Administration',
+            'Topic :: Utilities',
+            'Programming Language :: Python',
+            'Operating System :: POSIX :: Linux',
+            'License :: OSI Approved :: MIT License'
+            ]
         )
